fd48d3339b6980636d13619773b95864a2041e14
Balaji-Nandure <balajirn.2016@gmail.com>
2022-11-14 15:30:52 +0530

    hii

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pagination';
+  currentPage: number = 0;
+
+  images = [
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'At the Himalayas',
+      url: 'https://images.unsplash.com/photo-1601618613229-ec7645fad6fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGltYWxheWF8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'In Love',
+      url: 'https://images.unsplash.com/photo-1535615615570-3b839f4359be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGxvdmV8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'Thinking about you',
+      url: 'https://images.unsplash.com/photo-1522435229388-6f7a422cd95b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8dGhpbmtpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+    },
+  ];
 }
