bbe7819436c972c1fcf798656ce63d79ddeb79e6
Balaji-Nandure <balajirn.2016@gmail.com>
2022-11-14 18:20:30 +0530

    hii

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,11 +19,175 @@ export class AppComponent {
     },
     {
       title: 'In Love',
-      url: 'https://images.unsplash.com/photo-1535615615570-3b839f4359be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGxvdmV8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+      url: 'https://images.unsplash.com/photo-1593100126453-19b562a800c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8aHVnfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'Thinking about you',
+      url: 'https://images.unsplash.com/photo-1522435229388-6f7a422cd95b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8dGhpbmtpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'At the Himalayas',
+      url: 'https://images.unsplash.com/photo-1601618613229-ec7645fad6fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGltYWxheWF8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'In Love',
+      url: 'https://images.unsplash.com/photo-1593100126453-19b562a800c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8aHVnfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'Thinking about you',
+      url: 'https://images.unsplash.com/photo-1522435229388-6f7a422cd95b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8dGhpbmtpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'At the Himalayas',
+      url: 'https://images.unsplash.com/photo-1601618613229-ec7645fad6fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGltYWxheWF8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'In Love',
+      url: 'https://images.unsplash.com/photo-1593100126453-19b562a800c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8aHVnfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'Thinking about you',
+      url: 'https://images.unsplash.com/photo-1522435229388-6f7a422cd95b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8dGhpbmtpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'At the Himalayas',
+      url: 'https://images.unsplash.com/photo-1601618613229-ec7645fad6fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGltYWxheWF8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'In Love',
+      url: 'https://images.unsplash.com/photo-1593100126453-19b562a800c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8aHVnfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'Thinking about you',
+      url: 'https://images.unsplash.com/photo-1522435229388-6f7a422cd95b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8dGhpbmtpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'At the Himalayas',
+      url: 'https://images.unsplash.com/photo-1601618613229-ec7645fad6fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGltYWxheWF8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'In Love',
+      url: 'https://images.unsplash.com/photo-1593100126453-19b562a800c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8aHVnfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'Thinking about you',
+      url: 'https://images.unsplash.com/photo-1522435229388-6f7a422cd95b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8dGhpbmtpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'At the Himalayas',
+      url: 'https://images.unsplash.com/photo-1601618613229-ec7645fad6fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGltYWxheWF8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'In Love',
+      url: 'https://images.unsplash.com/photo-1593100126453-19b562a800c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8aHVnfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'Thinking about you',
+      url: 'https://images.unsplash.com/photo-1522435229388-6f7a422cd95b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8dGhpbmtpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'At the Himalayas',
+      url: 'https://images.unsplash.com/photo-1601618613229-ec7645fad6fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGltYWxheWF8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'In Love',
+      url: 'https://images.unsplash.com/photo-1593100126453-19b562a800c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8aHVnfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'Thinking about you',
+      url: 'https://images.unsplash.com/photo-1522435229388-6f7a422cd95b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8dGhpbmtpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'At the Himalayas',
+      url: 'https://images.unsplash.com/photo-1601618613229-ec7645fad6fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGltYWxheWF8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'In Love',
+      url: 'https://images.unsplash.com/photo-1593100126453-19b562a800c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8aHVnfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'Thinking about you',
+      url: 'https://images.unsplash.com/photo-1522435229388-6f7a422cd95b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8dGhpbmtpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'At the Himalayas',
+      url: 'https://images.unsplash.com/photo-1601618613229-ec7645fad6fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGltYWxheWF8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'In Love',
+      url: 'https://images.unsplash.com/photo-1593100126453-19b562a800c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8aHVnfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'Thinking about you',
+      url: 'https://images.unsplash.com/photo-1522435229388-6f7a422cd95b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8dGhpbmtpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'At the Himalayas',
+      url: 'https://images.unsplash.com/photo-1601618613229-ec7645fad6fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGltYWxheWF8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'In Love',
+      url: 'https://images.unsplash.com/photo-1593100126453-19b562a800c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8aHVnfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'Thinking about you',
+      url: 'https://images.unsplash.com/photo-1522435229388-6f7a422cd95b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8dGhpbmtpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'At the Himalayas',
+      url: 'https://images.unsplash.com/photo-1601618613229-ec7645fad6fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGltYWxheWF8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+    },
+    {
+      title: 'In Love',
+      url: 'https://images.unsplash.com/photo-1593100126453-19b562a800c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8aHVnfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60',
     },
     {
       title: 'Thinking about you',
       url: 'https://images.unsplash.com/photo-1522435229388-6f7a422cd95b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8dGhpbmtpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
     },
   ];
+
+  checkWindowIndex(index: number) {
+    return Math.abs(this.currentPage - index) < 5;
+  }
 }
